7301ffc9bb43738028334a668a226f6b828e72a9
aristapanell-cell <aristapanell@gmail.com>
2026-06-26 10:42:02 +0330

    Update and rename Arista/main.py to main.py

--- main.py
+++ main.py
@@ -32,8 +32,11 @@ def ensure_output():
 
 
 def load_config():
-    config_path = os.path.join(os.path.dirname(__file__), "config.json")
-    with open(config_path, "r", encoding="utf-8") as f:
+    with open(
+        "config.json",
+        "r",
+        encoding="utf-8"
+    ) as f:
         return json.load(f)
 
 
