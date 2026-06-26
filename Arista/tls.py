76df5a6041b8b4d66716706cc54e5318728d5790
aristapanell-cell <aristapanell@gmail.com>
2026-06-26 09:05:39 +0330

    Update tls.py

--- Arista/tls.py
+++ Arista/tls.py
@@ -2,9 +2,11 @@ import ssl
 import socket
 import hashlib
 import json
+import os
 
 def load_config():
-    with open("config.json", "r", encoding="utf-8") as f:
+    config_path = os.path.join(os.path.dirname(__file__), "config.json")
+    with open(config_path, "r", encoding="utf-8") as f:
         return json.load(f)
 
 def cert_meta(cert_bin, cert):
